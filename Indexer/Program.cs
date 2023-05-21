1de4168b54e479212831920d9bc105269561b35f
shrooqalsaadi <130554135+shrooqalsaadi@users.noreply.github.com>
2023-05-21 16:43:45 +0400

    Add files via upload
    Indexer>>>

---
+++ Indexer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            phoneBook b1 = new phoneBook (7);
+            b1[1, "sh"] = "345";
+            b1[2, "joy"] = "987";
+            Console.WriteLine(b1.getValue( "sh"));
+
+        }
+    }
+}
---
+++ Indexer/phoneBook.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Indexer
+{
+    internal class phoneBook
+    {
+
+        public string[] Name;
+        public string[] Number;
+        public int size;
+        int counter = 0;
+
+        public phoneBook(int size)
+        {
+            this.size = size;
+            this.Name = new string[this.size];
+            this.Number = new string[this.size];
+
+        }
+
+        public string this[int idx, string Name]
+        {
+            get
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (this.Name[i] == Name)
+                    {
+                        return this.Number[i];
+                    }
+                }
+                return "NOOOOOT THERE";
+
+            }
+            set
+            {
+                Number[idx] = value;
+                this.Name[idx] = Name;
+
+            }
+        }
+            public string this[string Name]
+        {
+            get
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (this.Name[i] == Name)
+                    {
+                        return this.Number[i];
+                    }
+                }
+                return "NOOOOOT THERE";
+
+            }
+        }
+            public void setValues(int index, string name, string numbers)
+        {
+            //validation
+            if ((index > 0) && (index < size) && (index > counter))
+            {
+                this.Number[index] = numbers;
+                this.Name[index] = name;
+                counter++;
+            }
+        }
+
+        public string getValue(string name)
+        {
+            //? for check
+            for (int i = 0; i < name?.Length; i++)
+            {
+                if (this.Name[i] == name)
+                {
+                    return Number[i];
+                }
+
+
+            }
+            return "not found";
+
+        }
+
+    }
+
+
+}
+
