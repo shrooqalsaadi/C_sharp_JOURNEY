cce8edc567c5bee146db77db2053638bb6b41488
shrooqalsaadi <130554135+shrooqalsaadi@users.noreply.github.com>
2023-05-26 11:26:32 +0400

    Indexer>>New
    ( the program show FavorateBook )>> give you the name of Author(when just write name of book)

---
+++ indexer new/Favouritbook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexer_new
+{
+    internal class Favouritbook
+    {
+        private string[] bookname;
+        private string[] Author;
+        private int Len;
+
+
+        public Favouritbook(int Len)
+        {
+            this.Len = Len;
+            bookname = new string[Len];
+            Author = new string[Len];
+
+        }
+
+        public string this[int indexer, string Name]
+
+        {
+            set {
+                bookname[indexer] = Name;
+                Author[indexer] = value;
+
+
+            }
+
+
+            get
+            {
+                string res = $"This is book Name :{bookname[indexer]} The Author Name : {Author[indexer]} ";
+                return res;
+            }
+
+
+        }
+        public string this[string name]
+        {
+            get
+            {
+                for (int i = 0; i < this.bookname.Length; i++)
+                {
+                    if (this.bookname[i] == name)
+                        return $"{bookname[i]} {Author[i]}";
+                }
+                return "Not Found";
+            }
+        }
+
+    }
+    }
+
---
+++ indexer new/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace indexer_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Favouritbook book = new Favouritbook (8);
+            book[1, "sunshine evrey day"] = "joy";
+            book[2, "ask why"] = "Temothy";
+            book[3, "Hw to kill Man"] = "Agatha";
+            Console.WriteLine(book["sunshine evrey day"]);
+
+        }
+    }
+}
