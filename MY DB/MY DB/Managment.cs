ec643dee0cc979b21993d62fd7daae79cb376c26
shrooqalsaadi <130554135+shrooqalsaadi@users.noreply.github.com>
2023-06-19 12:02:57 +0400

    MY DATA-BASE Manager

---
+++ MY DB/MY DB/Managment.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MY_DB
+{
+    internal class Managment : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            base.OnConfiguring(options);
+            options.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MY DB/MY DB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MY_DB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
