52db3412f4bf534e5dc154b4f1fd108e775e3562
shrooqalsaadi <130554135+shrooqalsaadi@users.noreply.github.com>
2023-06-19 12:15:28 +0400

    ReversFunction+ Extinction

---
+++ ReversFunction/ReversFunction/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+
+namespace ReversFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst = new List<int>() { 2, 4, 7, 9, 10 };
+
+            //Way 1 ...
+
+            var r = lst.Where(i => i % 2 == 0);
+            foreach (var item in r)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("");
+
+            //Way 2
+
+            var res = from i in lst
+                      where i % 2 == 0
+                      select i;
+            foreach (var item in res)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine("");
+
+        }
+
+        //    {
+        //        int s = 258;
+        //        int j = s.miroor();
+        //        Console.WriteLine(j);
+        //    }
+        //}
+
+
+    }
+}
---
+++ ReversFunction/ReversFunction/extintion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ReversFunction
+//{
+    //internal static class extintion
+
+    //{
+    //    public static int miroor(this int s)
+    //    {
+    //        char[] j = s.ToString().ToCharArray();
+    //        Array.Reverse(j);
+    //        return int.Parse(new string(j));
+    //    }
+    //}
+//}
